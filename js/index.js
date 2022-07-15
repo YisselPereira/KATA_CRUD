7675f6447ca5fa6aa34660a99fbdfaee486872f6
Yissel <yisselpereirar@gmail.com>
2022-07-15 19:02:10 -0300

    Segundo commit - Se agrego funciones de crear, editar y eliminar

--- js/index.js
+++ js/index.js
@@ -28,7 +28,99 @@ const getAll = async() =>{
         $table.querySelector("tbody").appendChild($fragment);
     } catch(err){
         let message = err.statusText || "Ocurrio un error";
-        $table.insertAdjacentHTML("afterend", '<p><b>Error ${err.status}: ${message}</b></p>');
+        $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
     }
 }
 d.addEventListener("DOMContentLoaded",getAll);
+
+d.addEventListener("submit", async e=>{
+    if(e.target === $form){
+        e.preventDefault();
+
+        if(!e.target.id.value){
+            //Create-POST
+            try{
+                let options = {
+                    method:"POST",
+                    headers:{
+                        "Content-type":"application/json;charset=utf-8"
+                    },
+                    body:JSON.stringify({
+                        nombre:e.target.nombre.value,
+                        desarrollador:e.target.desarrollador.value
+                    })
+                },
+                res = await fetch("http://localhost:3000/videojuegos", options)
+                json = await res.json();
+                
+
+                if(!res.ok) throw{
+                    status: res.status, statusText: res.statusText
+                    
+                };
+                location.reload();
+            }catch(err){
+                let message = err.statusText || "Ocurrio un error";
+                $form.insertAdjacentHTML("afterend0",`<p><b>Error ${err.status}:${message}</b></p>`);
+            }
+        } else{
+            //Update-PUT
+            try{
+                let options = {
+                    method: "PUT",
+                    headers:{
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                    body:JSON.stringify({
+                        nombre:e.target.nombre.value,
+                        desarrollador:e.target.desarrollador.value
+                    })
+                },
+                res = await fetch(`http://localhost:3000/videojuegos/${e.target.id.value}`, options),
+                json = await res.json();
+
+                if(!res.ok) throw{
+                    status:res.status, statusText:res.statusText
+                };
+                location.reload();
+            } catch(err){
+                let message = err.statusText || "Ocurrio un error";
+                $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
+            }
+        }
+    }
+});
+d.addEventListener("click", async e=>{
+    if(e.target.matches(".edit")){
+        $title.textContent = "Editar Videojuego";
+        $form.nombre.value = e.target.dataset.name;
+        $form.desarrollador.value = e.target.dataset.desarrollador;
+        $form.id.value = e.target.dataset.id;
+    }
+
+    if(e.target.matches(".delete")){
+        let isDelete = confirm(`Estas seguro de eliminar el id ${e.target.dataset.id}?`);
+
+        if(isDelete){
+            //Delete - DELETE
+            try{
+                let options = {
+                    method: "DELETE",
+                    headers:{
+                        "Content-type": "application/json; charset=utf-8"
+                    },
+                },
+                res = await fetch(`http://localhost:3000/videojuegos/${e.target.dataset.id}`, options),
+                json = await res.json();
+
+                if(!res.ok) throw{
+                    status:res.status, statusText:res.statusText
+                };
+                location.reload();
+            } catch(err){
+                let message = err.statusText || "Ocurrio un error";
+                alert(`Error ${err.status}: ${message}`);
+            }
+        }
+    }
+})
