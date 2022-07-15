eaf9c75f9aa9641b4bebbc1037f1b0ea19196407
Yissel <yisselpereirar@gmail.com>
2022-07-15 20:24:48 -0300

    Tercer commit - se agregaron estilos y comentarios

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//inicializamos
 const d = document,
 $table = d.querySelector(".crud-table"),
 $form = d.querySelector(".crud-form"),
@@ -5,15 +6,19 @@ $title = d.querySelector(".crud-title"),
 $template = d.getElementById("crud-template").content,
 $fragment = d.createDocumentFragment();
 
+// funcion para traer todos los datos
 const getAll = async() =>{
     try{
+        //consulta a la API para trer todos los videojuegos
         let res = await fetch("http://localhost:3000/videojuegos")
         json = await res.json();
 
+        //verifica si tiene algun error
         if(!res.ok) throw{
             status: res.status, statusText: res.statusText
         };
         console.log(json);
+        //recorre cada videojuego y lo agrega a la tabla
         json.forEach(el =>{
             $template.querySelector(".name").textContent = el.nombre;
             $template.querySelector(".desarrollador").textContent = el.desarrollador;
@@ -25,18 +30,21 @@ const getAll = async() =>{
             let $clone = d.importNode($template, true);
             $fragment.appendChild($clone);
         });
+        //agrega a el cuerpo de la tabla todas las filas de videojuegos
         $table.querySelector("tbody").appendChild($fragment);
     } catch(err){
         let message = err.statusText || "Ocurrio un error";
         $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`);
     }
 }
+//se ejecuta cuando se carga el documento y obtiene todos los datos
 d.addEventListener("DOMContentLoaded",getAll);
 
+//se ejecuta cuando se hace el submit del formulario
 d.addEventListener("submit", async e=>{
     if(e.target === $form){
         e.preventDefault();
-
+        //si no tiene valor de id, lo crea con el POST
         if(!e.target.id.value){
             //Create-POST
             try{
@@ -45,6 +53,7 @@ d.addEventListener("submit", async e=>{
                     headers:{
                         "Content-type":"application/json;charset=utf-8"
                     },
+                    //en el body se pasa el videojuego en formato JSON
                     body:JSON.stringify({
                         nombre:e.target.nombre.value,
                         desarrollador:e.target.desarrollador.value
@@ -58,12 +67,14 @@ d.addEventListener("submit", async e=>{
                     status: res.status, statusText: res.statusText
                     
                 };
+                //al finalizar carga la pagina de nuevo
                 location.reload();
             }catch(err){
                 let message = err.statusText || "Ocurrio un error";
                 $form.insertAdjacentHTML("afterend0",`<p><b>Error ${err.status}:${message}</b></p>`);
             }
         } else{
+            //en caso de que exista el id, actualiza el videojuego con el PUT
             //Update-PUT
             try{
                 let options = {
@@ -90,6 +101,8 @@ d.addEventListener("submit", async e=>{
         }
     }
 });
+//se ejecuta cuando se hace click en el documento
+//funcion async para poder usar el await del fecth
 d.addEventListener("click", async e=>{
     if(e.target.matches(".edit")){
         $title.textContent = "Editar Videojuego";
